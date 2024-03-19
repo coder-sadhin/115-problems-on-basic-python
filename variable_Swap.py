7477855ff065da5d1f9cb0ac721ed5f14d5ffd17
akkas_dev <codersadhin@gmail.com>
2024-03-19 13:14:29 +0800

    num1 problem solved

---
+++ variable_Swap.py
@@ -0,0 +1,10 @@
+# problem: Write a Python program to swap the values of two variables without using a temporary variable.
+
+num1=int(input("please enter your first number.   "))
+num2=int(input("please enter your secound number.   "))
+
+i=num1
+num1=num2
+num2=i
+
+print(f'your swap first number is {num1} and secound number is {num2}')

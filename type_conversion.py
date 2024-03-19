d7af6ab741904f80fa1dbe1cfefa72b6751ad8e1
akkas_dev <codersadhin@gmail.com>
2024-03-19 13:37:11 +0800

    num4 problem solved

---
+++ type_conversion.py
@@ -0,0 +1,5 @@
+# problem: 4. Type Conversion: Given a list of integers, write a Python program to convert each element of the list to a string.
+
+list=[1, 12, 15, 21, 131]
+list_str=[str(x) for x in list]
+print(list_str)

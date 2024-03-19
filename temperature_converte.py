ef62a2b8ab4252e5d366f6f5dd6c8df4a14c6784
akkas_dev <codersadhin@gmail.com>
2024-03-19 13:43:29 +0800

    num5 problem solved

---
+++ temperature_converte.py
@@ -0,0 +1,8 @@
+# problem: 5. Temperature Converter: 
+# Write a Python program that converts a temperature in Celsius to Fahrenheit. Take the Celsius temperature as input from the user.
+
+celsius=float(input("Enter temperature as Celsius"))
+
+fahrenheit=((9/5)*celsius)+32
+
+print(fahrenheit)

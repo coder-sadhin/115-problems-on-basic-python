acd1f29562cf9493be28d888740598aa81ce10d9
akkas_dev <codersadhin@gmail.com>
2024-03-19 13:19:51 +0800

    num2 problem solved

---
+++ even_or_odd.py
@@ -0,0 +1,8 @@
+# problem: 2. Even or Odd: Write a Python program that takes an integer as input and prints whether it is even or odd
+
+number=int(input('Enter your number.   '))
+
+if (number % 2) == 0 :
+    print(f"Your numer {number} is Even.")
+else:
+    print(f"Your numer {number} is Odd.")

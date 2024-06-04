afb114172ea21ede0aa66ad1e95d1731c5cbd724
akkas_dev <codersadhin@gmail.com>
2024-06-04 22:11:47 +0800

    create a calculor

---
+++ data_type_checker.py
@@ -0,0 +1,21 @@
+# problem: 6. Data Type Checker: Write a Python function that takes a variable as input 
+# and returns the data type of the variable as a string (e.g., “int”, “float”, “str”, “list”, etc.).
+
+# value=list(input())
+# print(type(value))
+
+intiger=int(input())
+found_pair = False
+for root in range(2,intiger):
+    for pwr in range(1,6):
+
+        if root**pwr == intiger:
+            found_pair = True
+            break
+    if found_pair:
+            break
+if found_pair:
+    print("Root: ", root, " & Power: " , pwr)
+else:
+     print("No pair integar exits")
+    

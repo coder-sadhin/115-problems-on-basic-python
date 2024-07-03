1f94d1a508ed1ed9662e25286d88c4419bddb98b
akkas_dev <codersadhin@gmail.com>
2024-07-03 22:46:43 +0800

    changing code 7

--- temperature_converte.py
+++ temperature_converte.py
@@ -6,3 +6,8 @@ celsius=float(input("Enter temperature as Celsius"))
 fahrenheit=((9/5)*celsius)+32
 
 print(fahrenheit)
+# celsius=float(input("Enter temperature as Celsius"))
+
+# fahrenheit=((9/5)*celsius)+32
+
+# print(fahrenheit)

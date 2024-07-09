43099fb05811b56a0cb50949638d2f49f883ae67
akkas_dev <codersadhin@gmail.com>
2024-07-09 23:10:35 +0800

    change some code

--- voice_ass.py
+++ voice_ass.py
@@ -71,6 +71,9 @@ if __name__ == '__main__':
         elif 'open spotify' in query:
             speak("opening spotify")
             webbrowser.open("spotify.com")
+        # elif 'open spotify' in query:
+        #     speak("opening spotify")
+        #     webbrowser.open("spotify.com")
         # elif 'open whatsapp' in query:
         #     speak("opening whatsapp")
         #     loc = "C:\\Users\\jaspr\\AppData\\Local\\WhatsApp\\WhatsApp.exe"

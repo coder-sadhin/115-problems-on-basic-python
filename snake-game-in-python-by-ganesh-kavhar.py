42b182204b3a7265b75a9cd1902b7e4a46d9023c
akkas_dev <codersadhin@gmail.com>
2024-07-09 23:09:10 +0800

    change some code

--- snake-game-in-python-by-ganesh-kavhar.py
+++ snake-game-in-python-by-ganesh-kavhar.py
@@ -13,6 +13,9 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
+# food = vector(0, 0)
+# snake = [vector(10, 0)]
+# aim = vector(0, -10)
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)

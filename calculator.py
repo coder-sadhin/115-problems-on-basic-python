f026211287210d45801491db747ea4c577e3c28a
AkkasDev <codersadhin@gmail.com>
2024-07-23 22:38:05 +0800

    chnage some

--- calculator.py
+++ calculator.py
@@ -25,8 +25,6 @@ while True:
 
     if user_input == "exit":
         break
-    # if user_input == "exit":
-    #     break
 
     if user_input in ("add", "subtract", "multiply", "divide"):
         num1 = float(input("Enter first number: "))
